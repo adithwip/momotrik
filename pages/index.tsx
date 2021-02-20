1d5838201d08220592fee50b49a8094189e45a6e
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2021-02-20 11:42:52 +0700

    fix: clean up unused files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,10 @@
-import Link from 'next/link'
+import { NextPage } from 'next'
+
 import Layout from '../components/Layout'
 
-const IndexPage = () => (
+const IndexPage: NextPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
+    <h1>Title</h1>
   </Layout>
 )
 
