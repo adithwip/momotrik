009f45b25b90bee9ff3bdd8d2c36e3bd98d4d77e
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2021-02-20 22:09:56 +0700

    feat: create custom hooks for getAllPosts in useGetAllPosts

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,12 @@ import type { GetAllPostsResponse } from '../interfaces/lib/getAllPosts.interfac
 
 import * as React from 'react'
 import { NextPage } from 'next'
-import { QueryClient, useQuery } from "react-query";
-import { dehydrate } from "react-query/hydration";
+import { QueryClient } from "react-query"
+import { dehydrate } from "react-query/hydration"
 
 import Layout from '../components/Layout'
 import { getAllPosts } from '../lib/getAllPosts'
+import { useGetAllPosts } from '../lib/useGetAllPosts'
 interface Props {
   allPosts: GetAllPostsResponse
 }
@@ -15,7 +16,6 @@ interface Props {
 export const getStaticProps: GetStaticProps = async () => {
   const queryClient = new QueryClient()
   await queryClient.prefetchQuery('posts', () => getAllPosts())
-  // const allPosts = await getAllPosts()
 
   return {
     props: {
@@ -25,23 +25,19 @@ export const getStaticProps: GetStaticProps = async () => {
 }
 
 const IndexPage: NextPage<Props> = () => {
-  // const { posts: { edges } } = allPosts
-  const { data, isFetching, isError } = useQuery(
-    'posts',
-    () => getAllPosts()
-  )
+  const { getAllPostsData } = useGetAllPosts()
 
   return (
     <Layout title="Home | Next.js + TypeScript Example">
       <h1>Posts</h1>
-      {isFetching ? (
+      {getAllPostsData.isFetching ? (
         <h1>Fetching data...</h1>
       ) : null}
-      {isError ? (
+      {getAllPostsData.isError ? (
         <h1>Error, gan!</h1>
       ) : null}
 
-      {data?.posts.edges.map(({ node }, index) => (
+      {getAllPostsData.data?.posts.edges.map(({ node }, index) => (
         <React.Fragment key={index}>
           <h1>{node.title}</h1>
           <p>{node.excerpt}</p>
