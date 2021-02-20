68df00d538852a47124dde19a5d68278b30bce82
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2021-02-20 22:09:56 +0700

    feat: create custom hooks for getAllPosts in useGetAllPosts

---
+++ lib/useGetAllPosts.ts
@@ -0,0 +1,47 @@
+import type { GetAllPostsResponse } from '../interfaces/lib/getAllPosts.interface'
+
+import { useQuery } from "react-query"
+
+import { fetchAPI } from './fetchAPI'
+
+
+const getAllPostsFetcher = async (): Promise<GetAllPostsResponse> => {
+  const res = await fetchAPI({
+    query: `
+      query AllPosts {
+        posts(first: 20, where: { orderby: { field: DATE, order: DESC}}) {
+          edges {
+            node {
+              id
+              date
+              title
+              slug
+              excerpt
+              extraPostInfo {
+                authorExcerpt
+                thumbImage {
+                  mediaItemUrl
+                }
+              }
+            }
+          }
+        }
+      }
+    `
+  }
+  );
+
+  return res.data.data
+}
+
+export const useGetAllPosts = () => {
+  const { data, isError, isFetching } = useQuery('posts', () => getAllPostsFetcher())
+
+  return {
+    getAllPostsData: {
+      data,
+      isError,
+      isFetching
+    }
+  } as const
+}
