5be693e466ff55e393fd78c7b08ab36c09567ca0
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2021-02-20 21:42:27 +0700

    feat: setup base for react-query and hydration for dehydratedState

---
+++ pages/_app.tsx
@@ -0,0 +1,19 @@
+import type { AppProps } from 'next/app'
+import { QueryClient, QueryClientProvider } from "react-query"
+import { ReactQueryDevtools } from "react-query/devtools"
+import { Hydrate } from "react-query/hydration";
+
+const queryClient = new QueryClient()
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  )
+}
+
+export default MyApp
