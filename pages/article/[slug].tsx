e7f58fcdbe4ecab9083cf11b3cf597722732cba3
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2021-02-22 09:18:32 +0700

    feat: create [slug].tsx dynamic page and index.tsx improvement

---
+++ pages/article/[slug].tsx
@@ -0,0 +1,89 @@
+import type { NextPage, GetStaticPaths, GetStaticProps } from 'next'
+import type { GetSinglePostResponse } from 'interfaces/lib/getSinglePost.interface'
+
+import dynamic from 'next/dynamic'
+import sanitizeHtml from 'sanitize-html'
+import { useRouter } from 'next/router'
+
+const Layout = dynamic(() => import('components/Layout'))
+
+import { getSinglePostFetcher } from 'lib/useGetSinglePost'
+import { getAllPostSlugsFetcher } from 'lib/useGetAllPostSlugs'
+
+type Props = {
+  slug?: string,
+  postData: GetSinglePostResponse
+}
+
+const ArticlePage: NextPage<Props> = ({ postData }) => {
+  const router = useRouter()
+
+  // What's happening here, is that we're looking at the isFallback property
+  // to determine if this page being rendered is a fallback version
+  // and if it's not, but we also don't have a slug,
+  // then this means we won't be able to render a page for this route.
+
+  // Instead of showing a horrible error page,
+  // we're going to return a simple paragraph with an error message in.
+  if (!router.isFallback && !postData?.post.slug) {
+    return <div>Looks like an error. Please reload...</div>
+  }
+
+  // If the page is not yet generated, this will be displayed
+  // initially until getStaticProps() finishes running
+  // Read more here: https://nextjs.org/docs/basic-features/data-fetching#fallback-pages
+  if (router.isFallback) {
+    return <div>Article is currently generated. Please wait...</div>
+  }
+
+  if (postData) {
+    const { post } = postData
+    return (
+      <Layout title={`${post.title} | Momotrik`}>
+        <article>
+          <h1 className="text-2xl font-bold text-gray-900 mb-8">{post.title}</h1>
+          <div dangerouslySetInnerHTML={{ __html: sanitizeHtml(post.content) }} />
+        </article>
+      </Layout>
+    )
+  }
+
+  return <p>Getting data...</p>
+}
+
+/**
+ * In Next.js if we want to statically generate a page with dynamic route (e.g [slug].tsx)
+ * The best result based on performance is using buil-in getStaticPaths and getStaticProps
+ * without intervention from other data fetching library like React Query.
+ * 
+ * So in this dynamic page, we use pure Next.js approach.
+ * Without even worry about caching and data management,
+ * like we use in other non-dynamic-route page with React Query dehydratedState.
+ */
+export const getStaticPaths: GetStaticPaths = async () => {
+  const allPosts = await getAllPostSlugsFetcher()
+
+  const paths = allPosts.posts.edges.map(({ node }) => ({
+    params: { slug: node.slug }
+  }))
+
+  return {
+    paths,
+    fallback: true
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const res = await getSinglePostFetcher(params?.slug)
+
+  return {
+    props: {
+      postData: res,
+      slug: params?.slug
+    },
+    revalidate: 1
+  }
+}
+
+export default ArticlePage
+
