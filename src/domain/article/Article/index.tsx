d8478cf4e5d8ff7be7052e2b4b70036b04449ecc
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2021-02-23 13:16:56 +0700

    hotfix: add more config to sanitize-html to allow img tag

--- src/domain/article/Article/index.tsx
+++ src/domain/article/Article/index.tsx
@@ -1,6 +1,6 @@
 import type { GetSinglePostResponse } from 'interfaces/lib/getSinglePost.interface'
 
-import sanitizeHtml from 'sanitize-html'
+import sanitizeHtml, { defaults } from 'sanitize-html'
 
 import Date from 'domain/article/Date'
 import styles from './Article.module.css'
@@ -20,10 +20,48 @@ const Article = ({ postData }: Props) => {
         </header>
         <Date date={post.date} />
 
+        
         <div
           className={styles.content}
           dangerouslySetInnerHTML={{
-            __html: sanitizeHtml(post.content)
+            __html: sanitizeHtml(post.content, {
+              /**
+               * <img> tag is not allowed by default
+               * so we need to adjust config and merged it with defaults
+               * see here: https://github.com/apostrophecms/sanitize-html#default-options
+               * 
+               * From the lib maintainer:
+               * "The syntax of poorly closed <p> and <img> elements is cleaned up."
+               * 
+               * So, basically content (by default) from WordPress
+               * is claimed "poorly closed" by them LOL
+               */
+              allowedTags: [...defaults.allowedTags, 'img'],
+              selfClosing: [...defaults.selfClosing, 'img'],
+              allowedAttributes: {
+                ...defaults.allowedAttributes,
+                img: [
+                  'data-attachment-id',
+                  'data-permalink',
+                  'data-orig-file',
+                  'data-orig-size',
+                  'data-comments-opened',
+                  'data-image-meta',
+                  'data-image-title',
+                  'data-image-description',
+                  'data-medium-file',
+                  'data-large-file',
+                  'loading',
+                  'width',
+                  'height',
+                  'src',
+                  'alt',
+                  'srcset',
+                  'sizes',
+                  'data-recalc-dims'
+                ]
+              }
+            })
           }} />
       </article>
     </main>
