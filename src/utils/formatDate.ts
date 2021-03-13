40aabdae538dd20b2360f6bbd2039af770a4b75a
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2021-03-13 11:32:33 +0700

    fix: add stricter type on options for formatDate utils

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,7 +1,13 @@
+type Options = {
+  year: "numeric" | "2-digit" | undefined,
+  month: "numeric" | "2-digit" | "long" | "short" | "narrow" | undefined,
+  day: "numeric" | "2-digit" | undefined
+}
+
 export const formatDate = (date: string) => {
   const event = new Date(date)
 
-  const options = {
+  const options: Options = {
     year: 'numeric',
     month: 'short',
     day: 'numeric'
