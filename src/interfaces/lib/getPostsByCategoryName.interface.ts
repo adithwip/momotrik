b9f442566d146d3e13d6acdcc8bfb0a77ed12be4
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2021-03-25 10:19:27 +0700

    feat: create lib to get all posts by category name

---
+++ src/interfaces/lib/getPostsByCategoryName.interface.ts
@@ -0,0 +1,37 @@
+export interface GetPostsByCategoryNameResponse {
+  posts: Posts
+}
+
+interface Posts {
+  edges: Edge[]
+}
+
+export interface Edge {
+  node: AllPostsNode
+}
+
+export interface AllPostsNode {
+  id: string
+  date: string
+  title: string
+  slug: string
+  excerpt: string
+  featuredImage?: FeaturedImage
+  author: Author
+}
+
+interface FeaturedImage {
+  node: FeaturedImageNode
+}
+
+interface FeaturedImageNode {
+  mediaItemUrl: string
+}
+
+export interface Author {
+  node: AuthorNode
+}
+
+export interface AuthorNode {
+  name: string
+}
