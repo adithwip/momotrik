f1188cde3fda49cc5cfb01fc96bd1db1e4d97e08
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2021-04-28 16:48:54 +0700

    feat: add webpack5 option to config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  future: {
+    webpack5: true,
+  },
   images: {
     domains: ['momotrikwp.wpcomstaging.com', 'secure.gravatar.com'],
     deviceSizes: [425, 640, 768, 1024, 1280, 1536]
