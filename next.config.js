ae50a169f71e04079a47310bb9b40f010bbb6ee2
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2021-04-28 15:05:37 +0700

    Revert "feat: add dynamic sitemaps without external data source"
    This reverts commit a0b4ecb8a394769c36f61b56abeca9c42ffa19a9.

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
 module.exports = {
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      require('./scripts/generate-sitemap');
-    }
-
-    return config;
-  },
   images: {
     domains: ['momotrikwp.wpcomstaging.com', 'secure.gravatar.com'],
     deviceSizes: [425, 640, 768, 1024, 1280, 1536]
