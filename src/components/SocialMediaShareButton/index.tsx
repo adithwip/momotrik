3b384861bce0df92eb285363169529989c439232
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2021-05-18 18:38:51 +0700

    fix: remove id from SocialMediaShareButton

--- src/components/SocialMediaShareButton/index.tsx
+++ src/components/SocialMediaShareButton/index.tsx
@@ -9,7 +9,7 @@ const SocialMediaShareButton = () => {
   }
 
   return (
-    <button onClick={handleFacebookShare} type="button" className={styles.button} id="social-media-share-button">
+    <button onClick={handleFacebookShare} type="button" className={styles.button}>
       Share to Facebook
       <svg className="ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
         <path d="M15 8a3 3 0 10-2.977-2.63l-4.94 2.47a3 3 0 100 4.319l4.94 2.47a3 3 0 10.895-1.789l-4.94-2.47a3.027 3.027 0 000-.74l4.94-2.47C13.456 7.68 14.19 8 15 8z" />
