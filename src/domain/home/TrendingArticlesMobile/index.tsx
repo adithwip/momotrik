8174bf9f631144f100124891483e7cec8cf3db3a
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2021-05-22 22:47:15 +0700

    fix: various warning fixes

--- src/domain/home/TrendingArticlesMobile/index.tsx
+++ src/domain/home/TrendingArticlesMobile/index.tsx
@@ -34,7 +34,7 @@ const TrendingArticlesMobile = () => {
 
                     <div className="absolute left-2 top-2 shadow bg-white p-1 rounded">
                       <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 text-green-400" viewBox="0 0 20 20" fill="currentColor">
-                        <path fillRule="evenodd" d="M12 7a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0V8.414l-4.293 4.293a1 1 0 01-1.414 0L8 10.414l-4.293 4.293a1 1 0 01-1.414-1.414l5-5a1 1 0 011.414 0L11 10.586 14.586 7H12z" clip-rule="evenodd" />
+                        <path fillRule="evenodd" d="M12 7a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0V8.414l-4.293 4.293a1 1 0 01-1.414 0L8 10.414l-4.293 4.293a1 1 0 01-1.414-1.414l5-5a1 1 0 011.414 0L11 10.586 14.586 7H12z" clipRule="evenodd" />
                       </svg>
                     </div>
 
