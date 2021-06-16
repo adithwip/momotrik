fe096ca489e7ae1b6a449beb58b865132cbcc43a
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2021-06-16 11:54:02 +0700

    chore: update libraries next 11

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-  future: {
-    webpack5: true,
-  },
   images: {
     domains: ['momotrikwp.wpcomstaging.com', 'secure.gravatar.com'],
     deviceSizes: [425, 640, 768, 1024, 1280, 1536]
