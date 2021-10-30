f465125491b7eb1b578d6aa9d6b19b5c09907d0d
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2021-10-30 20:21:03 +0700

    chore: upgrade libraries, next/image avif opt-in, bumb node version

--- next.config.js
+++ next.config.js
@@ -2,5 +2,6 @@ module.exports = {
   images: {
     domains: ['momotrikwp.wpcomstaging.com', 'secure.gravatar.com'],
     deviceSizes: [425, 640, 768, 1024, 1280, 1536],
+    formats: ['image/avif', 'image/webp'],
   },
 }
