fd4457c5df511e5bb230a0e9524e843ee98606a2
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2021-11-14 13:44:00 +0700

    style: change font from Cairo to Inter variable font

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Cairo', ...defaultTheme.fontFamily.sans],
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
       },
       minWidth: {
         unset: 'unset',
