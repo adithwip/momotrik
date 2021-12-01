69f8f88a5c066b17f4c684d6770a14cdcb88421c
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2021-12-01 17:02:45 +0700

    style: change web fonts to Rubik. Previously Inter

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+        sans: ['Rubik', ...defaultTheme.fontFamily.sans],
       },
       colors: {
         primary: {
