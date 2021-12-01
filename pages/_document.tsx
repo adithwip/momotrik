4ea47f3db5fdb303d3b9b04937b03bd46f5c9201
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2021-12-01 17:02:45 +0700

    style: change web fonts to Rubik. Previously Inter

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -28,7 +28,7 @@ class MyDocument extends Document {
             crossOrigin=""
           />
           <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;600;700&display=swap"
             rel="stylesheet"
           />
 
