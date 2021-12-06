17ef0a67f4ebc7c7a360f62b662673179aae7cec
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2021-12-06 11:18:14 +0700

    feat: revamp desktop and mobile posts list

--- src/components/Cards/ArticleCard/index.tsx
+++ src/components/Cards/ArticleCard/index.tsx
@@ -1,8 +1,9 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import clsx from 'clsx'
+import ReactMarkdown from 'react-markdown'
+import rehypeRaw from 'rehype-raw'
 
-import { stripHtmlTags } from 'utils/stripHtmlTags'
 import { formatDate } from 'utils/formatDate'
 
 import styles from './ArticleCard.module.css'
@@ -36,36 +37,15 @@ const ArticleCard = ({
       className={clsx(
         styles.articleWrapper,
         isHiddenOnDesktopView && 'md:hidden',
-        isNotLastChild && 'mb-6 md:mb-12',
+        isNotLastChild && 'mb-6 md:mb-0',
         isGrid && 'w-full md:min-w-300 md:max-w-300 m-2'
       )}
     >
       <Link href={`/article/${slug}`}>
         <a>
-          <figure className={styles.imageWrapper}>
-            <Image
-              alt={`${title} Image`}
-              src={mediaItemUrl}
-              sizes={sizes}
-              layout="fill"
-              objectFit="cover"
-              objectPosition="center"
-              quality={25}
-            />
-          </figure>
-
           <div className={styles.titleWrapper}>
             <div className="flex">
-              <div className="flex flex-col flex-1 py-2 pr-4 md:p-0 md:pl-0">
-                <p className={styles.articleTitle}>{title}</p>
-
-                <p className={styles.articleDate}>
-                  {`${formatDate(date)} | ${authorName}`}
-                </p>
-
-                <p className={styles.excerpt}>{stripHtmlTags(excerpt)}</p>
-              </div>
-              <div className={styles.mobileImageWrapper}>
+              <figure className={styles.imageWrapper}>
                 <Image
                   alt={`${title} Image`}
                   src={mediaItemUrl}
@@ -75,6 +55,20 @@ const ArticleCard = ({
                   objectPosition="center"
                   quality={25}
                 />
+              </figure>
+              <div className="flex flex-col flex-1 pl-4">
+                <p className={styles.articleTitle}>{title}</p>
+
+                <p className={styles.articleDate}>
+                  {`${formatDate(date)} | ${authorName}`}
+                </p>
+
+                <ReactMarkdown
+                  className={styles.excerpt}
+                  rehypePlugins={[rehypeRaw]}
+                >
+                  {excerpt}
+                </ReactMarkdown>
               </div>
             </div>
           </div>
--- src/utils/stripHtmlTags.ts
+++ src/utils/stripHtmlTags.ts
@@ -1,5 +1,5 @@
 export const stripHtmlTags = (str: string): string => {
-  const cleanStr = str.replace(/<\/?[^>]+(>|$)/g, '')
+  const cleanStr = str.replace(/<\/?[^>]+(>|$)/g, '').replace(/&hellip;/g, '')
 
   return cleanStr
 }
