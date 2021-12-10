60d9f02573495445f0a4a86483bacf6bc221321d
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2021-12-10 09:02:01 +0700

    fix: improve split html array to half utils

--- src/domain/article/utils/index.ts
+++ src/domain/article/utils/index.ts
@@ -7,12 +7,28 @@ const parseHTMLString = (htmlString: string) => {
 }
 const splitHTMLArrayToHalf = (htmlList: string[]) => {
   const half = Math.ceil(htmlList.length / 2)
-  const firstHalfMarkupList = htmlList.slice(0, half).join('')
-  const secondHalfMarkupList = htmlList.slice(-half).join('')
+  const firstHalfMarkupList = htmlList.slice(0, half)
+  const secondHalfMarkupList = htmlList.slice(-half)
+  let firstHalfHTMLString = ''
+  let secondHalfHTMLString = ''
+
+  /**
+   * If the last value of firstHalfList equal to first value of secondHalfList
+   * remove the first value of secondHalfList
+   */
+  if (
+    firstHalfMarkupList[firstHalfMarkupList.length - 1] ===
+    secondHalfMarkupList[0]
+  ) {
+    secondHalfMarkupList.shift()
+  }
+
+  firstHalfHTMLString = firstHalfMarkupList.join('')
+  secondHalfHTMLString = secondHalfMarkupList.join('')
 
   return {
-    firstHalfMarkupList,
-    secondHalfMarkupList,
+    firstHalfHTMLString,
+    secondHalfHTMLString,
   }
 }
 
@@ -25,11 +41,11 @@ export const parseAndSplitHTMLString = (markup: string): FunctionReturn => {
   const sanitizedMarkup = sanitizeHTMLString(markup)
   const markupList = parseHTMLString(sanitizedMarkup)
 
-  const { firstHalfMarkupList, secondHalfMarkupList } =
+  const { firstHalfHTMLString, secondHalfHTMLString } =
     splitHTMLArrayToHalf(markupList)
 
   return {
-    firstHalf: firstHalfMarkupList,
-    secondHalf: secondHalfMarkupList,
+    firstHalf: firstHalfHTMLString,
+    secondHalf: secondHalfHTMLString,
   }
 }
