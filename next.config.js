78ceb1444c2ad0c64375bad783b79cdd976c70eb
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2021-12-17 13:48:23 +0700

    chore: permanent redirection from /article/:slug to /artikel/:slug

--- next.config.js
+++ next.config.js
@@ -4,4 +4,13 @@ module.exports = {
     deviceSizes: [425, 640, 768, 1024, 1280, 1536],
     formats: ['image/avif', 'image/webp'],
   },
+  async redirects() {
+    return [
+      {
+        source: '/article/:slug',
+        destination: '/artikel/:slug',
+        permanent: true,
+      },
+    ]
+  },
 }
