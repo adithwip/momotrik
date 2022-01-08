cc778caf83976b411d4ef7400bda78226b5c4a94
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2022-01-08 17:48:01 +0700

    feat: fetch data for all posts rss feed and generateRssFeed utils

---
+++ src/lib/useGetAllPostsForRssFeed.ts
@@ -0,0 +1,45 @@
+import type { GetAllPostsForRssResponse } from 'interfaces/lib/getAllPostsForRssFeed'
+
+import { fetchAPI } from 'lib/fetchAPI'
+
+export const getAllPostsForRssFeed =
+  async (): Promise<GetAllPostsForRssResponse> => {
+    const res = await fetchAPI({
+      query: `
+      query AllPostsForRssFeed($count: Int = 30) {
+        posts(first: $count, where: {orderby: {field: DATE, order: DESC}}) {
+          edges {
+            node {
+              id
+              date
+              title
+              slug
+              excerpt
+              categories {
+                edges {
+                  node {
+                    name
+                  }
+                }
+              }
+              featuredImage {
+                node {
+                  mediaItemUrl
+                  sizes(size: MEDIUM_LARGE)
+                }
+              }
+              author {
+                node {
+                  name
+                  email
+                }
+              }
+            }
+          }
+        }
+      }
+    `,
+    })
+
+    return res.data.data
+  }
