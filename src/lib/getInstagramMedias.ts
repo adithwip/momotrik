583eddcee4cfc0de3be60e7ebfb0909abe543a4d
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2022-01-13 12:00:18 +0700

    feat: fetch instagram posts first try

---
+++ src/lib/getInstagramMedias.ts
@@ -0,0 +1,29 @@
+import type { GetInstagramMediasResponse } from 'interfaces/lib/getInstagramMedias'
+import type { AxiosResponse } from 'axios'
+
+import axios from 'axios'
+
+const RAPID_API_KEY = process.env.RAPID_API_KEY
+
+export const getInstagramMedias = async (): Promise<
+  AxiosResponse<GetInstagramMediasResponse>
+> => {
+  try {
+    const res = await axios({
+      url: 'https://instagram28.p.rapidapi.com/medias',
+      method: 'GET',
+      headers: {
+        'x-rapidapi-host': 'instagram28.p.rapidapi.com',
+        'x-rapidapi-key': RAPID_API_KEY as string,
+      },
+      params: {
+        user_id: 44778586320, // Momotrik Instagram user_id,
+        batch_size: 5,
+      },
+    })
+
+    return res
+  } catch (error) {
+    throw new Error(`Error: GET Instragram medias: ${error}`)
+  }
+}
