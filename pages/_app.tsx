4de8666526daf999b9e491faf9796926831abe6c
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2022-02-03 17:20:03 +0700

    feat: use next script for google adsense scripts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,32 +16,54 @@ const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter()
+  const isProduction = process.env.NODE_ENV === 'production'
 
   useEffect(() => {
-    if (process.env.NODE_ENV === 'production') {
+    if (isProduction) {
       router.events.on('routeChangeComplete', pageview)
       return () => {
         router.events.off('routeChangeComplete', pageview)
       }
     }
-  }, [router.events])
+  }, [isProduction, router.events])
 
   return (
     <>
-      {/* Google Tag Manager - Global base code */}
-      <Script
-        id="google-tag-manager-script"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer', '${GTM_ID}');
-          `,
-        }}
-      />
+      {/* Only run Google Adsense and Tag Manager scripts in Production Environment */}
+      {isProduction ? (
+        <>
+          {/* Google Tag Manager - Global base code */}
+          <Script
+            id="google-tag-manager-script"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer', '${GTM_ID}');
+            `,
+            }}
+          />
+
+          {/* Google Adsense scripts */}
+          <Script
+            id="google-adsense-pagead2-script"
+            strategy="afterInteractive"
+            data-ad-client="ca-pub-3226350239455992"
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+          />
+          <Script
+            id="google-adsense-script"
+            dangerouslySetInnerHTML={{
+              __html: `
+              (adsbygoogle = window.adsbygoogle || []).push({});
+            `,
+            }}
+          />
+        </>
+      ) : null}
       <QueryClientProvider client={queryClient}>
         <Hydrate state={pageProps.dehydratedState}>
           <Component {...pageProps} />
