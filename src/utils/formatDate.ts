062a0ffb6f97c5f76299548c8d4a2a5dab4928b3
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2022-02-03 14:10:03 +0700

    refactor: remove repeated type for utlis formatDate

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,13 +1,7 @@
-type Options = {
-  year: 'numeric' | '2-digit' | undefined
-  month: 'numeric' | '2-digit' | 'long' | 'short' | 'narrow' | undefined
-  day: 'numeric' | '2-digit' | undefined
-}
-
 export const formatDate = (date: string) => {
   const event = new Date(date)
 
-  const options: Options = {
+  const options: Intl.DateTimeFormatOptions = {
     year: 'numeric',
     month: 'short',
     day: 'numeric',
