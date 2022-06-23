bf6b853049fc3f61e511e9d111217f68fb90d9d8
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2022-06-23 09:11:31 +0700

    feat: create social media profile buttons for mobile view

---
+++ src/components/SocialMediaProfile/SocialMediaProfile.component.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import clsx from 'clsx'
+
+import styles from './SocialMediaProfile.module.css'
+
+const SocialMediaProfile = () => {
+  return (
+    <div className="mb-5 flex justify-between rounded-md border-2 border-white">
+      <a
+        className={clsx(styles.button, styles.instagram)}
+        href="https://www.instagram.com/momotrik.id/"
+        target="_blank"
+        role="button"
+        rel="noreferrer"
+      >
+        Instagram
+      </a>
+      <a
+        className={clsx(styles.button, styles.twitter)}
+        href="https://twitter.com/momotrik"
+        target="_blank"
+        role="button"
+        rel="noreferrer"
+      >
+        Twitter
+      </a>
+      <a
+        className={clsx(styles.button, styles.facebook)}
+        href="https://www.facebook.com/momotrik.ID"
+        target="_blank"
+        role="button"
+        rel="noreferrer"
+      >
+        Facebook
+      </a>
+    </div>
+  )
+}
+
+export default SocialMediaProfile
